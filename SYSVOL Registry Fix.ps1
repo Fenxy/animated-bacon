e6a1a5ad08fbf98b77b3438464524ae9057db044
John Burrows <fenxy@protonmail.ch>
2016-05-20 15:31:02 +0100

    SYSVOL reg fix

---
+++ SYSVOL Registry Fix.ps1
@@ -0,0 +1,9 @@
+﻿#SYSVOL Fix
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths -Name \\*\SYSVOL
+#Set-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths\\\*\SYSVOL -Value RequireMutualAuthentication=0
+
+#NETLOGON Fix
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths -Name \\*\NETLOGON
+#Set-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths\\\*\NETLOGON -Value RequireMutualAuthentication=0
+
+pause
