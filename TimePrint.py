fee46d4a42b1267a65d2920a06331de66bab6101
John Burrows <fenxy@protonmail.ch>
2016-05-20 12:51:45 +0100

    Line 5 indent

--- TimePrint.py
+++ TimePrint.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 now = datetime.now()
 print '%s/%s/%s %s:%s:%s' % (now.day, now.month, now.year, now.hour, now.minute, now.second)
-	input("prompt: ")
+input("prompt: ")
