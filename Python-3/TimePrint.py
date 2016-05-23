c91a3f8d548ed185e798b0a4f33a69168a163d12
John Burrows <fenxy@protonmail.ch>
2016-05-23 08:45:41 +0100

    Clock
    Made it work!

--- Python-3/TimePrint.py
+++ Python-3/TimePrint.py
@@ -1,14 +1,14 @@
-#Date/Time/Clock thingy
-
-from datetime import datetime #Get current time?
+from datetime import datetime
 import time
-import os #get host os
+import os
 
-now = datetime.now() #define now as datetime.now()
+now = datetime.now()
 
-x = 1 #this is always true
-while True: #infinite loop begin
-    from datetime import datetime
-    print("{0}/{1}/{2} {3}:{4}:{5}".format(now.day, now.month, now.year, now.hour, now.minute, now.second))
-    #line 12 should set out the time/date like DD/MM/YY HH:MM:SS there is a bug where if a number starts with 0 it is blank.
-    os.system('cls' if os.name == 'nt' else 'clear') #clear screen Win/Mac
+i = 0
+
+while i == 0:
+	now = datetime.now()
+	time.sleep(1)
+	os.system('CLS')
+	print("{0}/{1}/{2} {3}:{4}:{5}".format(now.day, now.month, now.year, now.hour, now.minute, now.second))
+	
